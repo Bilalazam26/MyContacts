453ce4ec1260b08c31fffedf74a48f95c616f39e
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-09 04:00:49 +0300

    Refactor: Move contact sync logic to AndroidContactsSyncManager

--- composeApp/src/androidMain/kotlin/com/bilalazzam/mycontacts/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/bilalazzam/mycontacts/MainActivity.kt
@@ -1,50 +1,22 @@
 package com.bilalazzam.mycontacts
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.work.Constraints
-import androidx.work.ExistingWorkPolicy
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.bilalazzam.contacts_provider.ContactsProvider
-import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        val syncManager = AndroidContactsSyncManager(this)
+        syncManager.enqueueSync()
         setContent {
             App(
-                contactsProvider = ContactsProvider(this)
+                contactsProvider = ContactsProvider(this),
+                syncManager = AndroidContactsSyncManager(this)
             )
-        }
-    }
-
-    companion object {
-        private const val SYNC_WORK_NAME = "contacts_sync_work"
-
-        fun enqueueOneTimeSync(context: Context) {
-            val constraints = Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build()
-
-            val syncRequest = OneTimeWorkRequestBuilder<ContactsSyncWorker>()
-                .setConstraints(constraints)
-                .build()
-
-            Log.d("ContactsApp", "🕒 Enqueuing one-time sync request...")
-
-            WorkManager.getInstance(context).beginUniqueWork(
-                SYNC_WORK_NAME,
-                ExistingWorkPolicy.REPLACE,
-                syncRequest
-            ).enqueue()
         }
     }
 }
