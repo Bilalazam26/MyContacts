078d8ee3fd2eb7ba878692924db57836a7030832
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-10 20:37:29 +0300

    Rename BackgroundWorkTaskIOSKt to registerTaskKt

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -14,7 +14,7 @@ struct iOSApp: App {
     class AppDelegate: NSObject, UIApplicationDelegate {
         func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
-            BackgroundWorkTaskIOSKt.registerTask()
+            registerTaskKt.registerTask()
 
             return true
         }
